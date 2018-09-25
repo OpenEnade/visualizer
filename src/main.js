023931ba8839c2cafe2488f2d4bea366d52d80e1
Emanuel Joivo <emanueljoivo@gmail.com>
2018-09-25 10:50:22 -0300

    Static analysis standards of Airbnb and bootstrap-vue plugin configured

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import '@babel/polyfill'
+/* eslint-disable import/no-extraneous-dependencies */
+import '@babel/polyfill';
 import Vue from 'vue';
-import './plugins/bootstrap-vue'
+import './plugins/bootstrap-vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
