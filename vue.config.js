891a385ec62e5b4426214257f6ebd558928bd35c
Emanuel Joivo <emanueljoivo@gmail.com>
2018-10-08 00:53:28 -0300

    Pull request template.

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: 'src/assets',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined,
+};
