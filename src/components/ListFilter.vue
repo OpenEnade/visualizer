aa7d405fe90020d17fe7b748d4df9aa1373e126b
igoratf <igor.atf@gmail.com>
2018-10-19 21:31:59 -0300

    create filter component

---
+++ src/components/ListFilter.vue
@@ -0,0 +1,146 @@
+<template lang="html">
+  <section class="filter">
+    <h1>Filtros</h1>
+  
+    <div class="form-row">
+      <div class="form-group col-sm-2 col-md-2 col-lg-2"></div>
+       <div class="form-group col-sm-2 col-md-2 col-lg-2">
+        <b-form-select
+          v-b-tooltip.hover
+          title="Filtrar por estado"
+          v-model="state"
+          :options="stateList"
+          class="mb-3"
+          size="sm"/>
+        <div>Selected: <strong>{{ state }}</strong></div>
+      </div>
+      <div class="form-group col-sm-2 col-md-2 col-lg-2">
+        <b-form-select
+          v-b-tooltip.hover
+          title="Filtrar por município"
+          v-model="city"
+          :options="cityList"
+          class="mb-3"
+          size="sm"/>
+        <div>Selected: <strong>{{ city }}</strong></div>
+      </div>
+      <div class="form-group col-sm-2 col-md-2 col-lg-2">
+        <b-form-select
+        v-b-tooltip.hover
+          title="Filtrar por categoria administrativa"
+          v-model="adminCategory"
+          :options="categoryList"
+          class="mb-3"
+          size="sm"/>
+        <div>Selected: <strong>{{ adminCategory }}</strong></div>
+      </div>
+       <div class="form-group col-sm-2 col-md-2 col-lg-2">
+        <b-form-select
+          v-b-tooltip.hover
+          title="Filtrar por modalidade de ensino"
+          v-model="educationModality"
+          :options="modalityList"
+          class="mb-3"
+          size="sm"/>
+        <div>Selected: <strong>{{ educationModality }}</strong></div>
+      </div>
+      <div class="form-group col-sm-2 col-md-2 col-lg-2"></div>
+    </div>
+  
+  </section>
+</template>
+
+<script lang="js">
+  export default {
+    name: 'ListFilter',
+    mounted() {
+  
+    },
+    data() {
+      return {
+        state: '',
+        city: '',
+        adminCategory: '',
+        educationModality: '',
+        stateList: [
+          {
+            value: '',
+            text: 'Estado'
+          },
+          {
+            value: 'Paraíba',
+            text: 'Paraíba'
+          },
+          {
+            value: 'Pernambuco',
+            text: 'Pernambuco'
+          },
+          {
+            value: 'Ceará',
+            text: 'Ceará'
+          },
+        ],
+        cityList: [
+          {
+            value: '',
+            text: 'Município'
+          },
+          {
+            value: 'Campina Grande',
+            text: 'Campina Grande'
+          },
+          {
+            value: 'Recife',
+            text: 'Recife'
+          },
+          {
+            value: 'Ceará',
+            text: 'Fortaleza'
+          },
+        ],
+        categoryList: [
+          {
+            value: '',
+            text: 'Categoria Administrativa'
+          },
+          {
+            value: 'Federal',
+            text: 'Federal'
+          },
+          {
+            value: 'Estadual',
+            text: 'Estadual'
+          },
+          {
+            value: 'Privada',
+            text: 'Privada'
+          },
+        ],
+        modalityList: [
+          {
+            value: '',
+            text: 'Modalidade de Ensino'
+          },
+          {
+            value: 'Presencial',
+            text: 'Presencial'
+          },
+          {
+            value: 'Educação à Distância',
+            text: 'Educação à Distância'
+          },
+        ],
+      }
+    },
+    methods: {
+  
+    },
+    computed: {
+  
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .filter {}
+</style>
