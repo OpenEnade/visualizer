d6f6931102083a3feecdf566f710de7d4e03a46b
paulovitorccc <paulo.dantas@ccc.ufcg.edu.br>
2018-11-04 19:24:35 -0300

    #task01 - Starting footer implementation.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="App">
     <CustomHeader />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
 import CustomHeader from '@/components/CustomHeader.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     CustomHeader,
+    Footer
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="footer">
+        <div class="container">
+            <h6>© 2018 OpenEnade</h6>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+    .footer {
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        padding: 1rem;
+        background-color: #efefef;
+        text-align: center;
+        color: #aec3d8;
+        width: 100%;
+    }
+    .doido {
+        position: sticky;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        padding: 10px;
+        background-color: #efefef;
+        text-align: center;
+    }
+
+</style>
