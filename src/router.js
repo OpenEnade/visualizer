891cfe21be40c71b731b8d887c77cd39cf683093
igoratf <igor.atf@gmail.com>
2018-11-15 15:01:54 -0300

    remove contact component and add comparison component

--- src/router.js
+++ src/router.js
@@ -4,7 +4,6 @@ import Courses from './views/Courses.vue';
 import Dashboard from './views/Dashboard.vue';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
-import Contact from './views/Contact.vue';
 import CourseDetail from './components/CourseDetail.vue';
 import PageHeader from './components/PageHeader.vue';
 
@@ -13,51 +12,46 @@ Vue.use(Router);
 export default new Router({
   mode: 'history',
   routes: [{
-    path: '*',
-    redirect: '/home',
-  },
-  {
-    path: '/',
-    redirect: '/home',
-    // redirect: '/detalhes',,
-  },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home,
-  },
-  {
-    path: '/detalhes',
-    name: 'detail',
-    component: CourseDetail,
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: About,
-  },
-  {
-    path: '/contact',
-    name: 'contact',
-    component: Contact,
-  },
-  {
-    path: '/cursos',
-    name: 'cursos',
-    component: Courses,
-  },
-  {
-    path: '/universidades',
-    name: 'universidades',
-    component: Dashboard,
-  },
-  {
-    path: '/sobre',
-    name: 'sobre',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-  },
+      path: '*',
+      redirect: '/home',
+    },
+    {
+      path: '/',
+      redirect: '/home',
+      // redirect: '/detalhes',,
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/detalhes',
+      name: 'detail',
+      component: CourseDetail,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About,
+    },
+    {
+      path: '/cursos',
+      name: 'cursos',
+      component: Courses,
+    },
+    {
+      path: '/universidades',
+      name: 'universidades',
+      component: Dashboard,
+    },
+    {
+      path: '/sobre',
+      name: 'sobre',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue'),
+    },
   ],
 });
---
+++ src/views/Comparison.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+
+  <section class="comparison">
+    <PageHeader description="Comparação de cursos" />
+    <h1>Comparação</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  import PageHeader from '@/components/PageHeader.vue';
+
+  export default  {
+    name: 'comparison',
+    components: {
+      PageHeader
+    },
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.comparison {
+}
+</style>
