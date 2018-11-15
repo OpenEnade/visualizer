1ace6f02f68d071acb0687428d4758c20f12def0
igoratf <igor.atf@gmail.com>
2018-11-15 14:45:44 -0300

    change home button color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,12 @@
     <br>
     <br>
     <b-container flex>
-      <div
-        class="row justify-content-md-center"
-        style="margin-top: 10%">
+      <div class="row justify-content-md-center" style="margin-top: 10%">
         <div class="col-md-auto">
           <h1 class="lead display-3 text-center">Open Enade</h1>
           <p class="lead text-center"> Plataforma aberta para consulta dos dados
-          do Exame Nacional de Desempenho de Estudantes </p>
-          <router-link
-            tag="button"
-            class="btn btn-primary btn-lg btn-block"
-            to="cursos">Pesquise sobre um curso!</router-link>
+            do Exame Nacional de Desempenho de Estudantes </p>
+          <router-link tag="button" class="btn btn-primary btn-lg btn-block" to="cursos">Pesquise sobre um curso!</router-link>
         </div>
       </div>
     </b-container>
@@ -22,9 +17,9 @@
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   components: {},
-  methods: {},
+  methods: {}
 };
 </script>
 
@@ -39,5 +34,15 @@ p {
 }
 button {
   margin-top: 5%;
+}
+
+.btn-primary {
+  background-color: #36648b;
+  border-color: #36648b;
+}
+
+.btn-primary:hover {
+  background-color: #104e8b;
+  border-color: #104e8b;
 }
 </style>
