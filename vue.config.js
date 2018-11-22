261eeb27f87730ba3feae0f49e8e3dce06b14a28
Emanuel Joívo <emanueljoivo@gmail.com>
2018-11-21 23:38:16 -0300

    Rebuild in the .igignore file.

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  baseUrl: undefined,
-  outputDir: undefined,
-  assetsDir: 'src/assets',
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined,
-};
