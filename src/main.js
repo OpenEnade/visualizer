07d1dce807a6ca07d311ba1fb5118887bc76fb24
Emanuel Joívo <emanueljoivo@gmail.com>
2018-11-22 00:14:00 -0300

    Dead dependencies removed.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,10 @@ import './plugins/axios';
 import './plugins/bootstrap-vue';
 import App from './App.vue';
 import router from './router';
-import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store,
   render: h => h(App),
 }).$mount('#App');
