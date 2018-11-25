3a352fa4ce6eea799d73d7a0c6939c7645df7edb
igoratf <igor.atf@gmail.com>
2018-11-24 23:28:17 -0300

    clear selected course when moving to course selection

--- src/components/CourseSelector.vue
+++ src/components/CourseSelector.vue
@@ -5,29 +5,16 @@
       <div class="form-row">
         <div class="form-group col-sm-3 col-md-3 col-lg-3" />
         <div class="form-group col-sm-6 col-md-6 col-lg-6 text-center">
-          <label
-            for="course-selection"
-            class="course-label">Cursos</label>
-          <select
-            id="course"
-            v-model="course"
-            class="form-control">
-            <option
-              disabled
-              selected
-              value="">Escolha um curso</option>
-            <option
-              v-for="curso in courseList"
-              :key="curso.codigoCurso"
-              :value="curso.nome">
+          <label for="course-selection" class="course-label">Cursos</label>
+          <select id="course" v-model="course" class="form-control">
+            <option disabled selected value="">Escolha um curso</option>
+            <option v-for="curso in courseList" :key="curso.codigoCurso" :value="curso.nome">
               {{ curso.nome }}
             </option>
 
           </select>
           <div class="btn-div">
-            <button
-              class="btn btn-primary"
-              @click.prevent="searchCourse()">Pesquisar</button>
+            <button class="btn btn-primary" @click.prevent="searchCourse()">Pesquisar</button>
           </div>
         </div>
         <div class="form-group col-sm-6 col-md-3 col-lg-3" />
@@ -54,6 +41,7 @@ export default {
     },
   },
   created() {
+    localStorage.clear();
     ApiService.getCourses()
       .then(response => this.courses = response.data);
   },
